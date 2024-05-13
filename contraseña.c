1dfac131094374781370d477c19c8b1a27c945a7
ailyn27sa <168689298+ailyn27sa@users.noreply.github.com>
2024-05-13 10:45:17 -0600

    examenversion4contraseña.c

--- contraseña.c
+++ contraseña.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-
-
 int main() {
-    int cantidad;
-    float precioUnitario;
-    float importeTotal = 0;
     char nombre[50];
     char contrasena[50];
     int intentos = 3;
+    int cantidad;
+    float precioUnitario;
+    float importeTotal = 0;
 
- do {
+   
+    do {
         printf("Introduce tu nombre: ");
         scanf("%s", nombre);
 
@@ -27,40 +26,38 @@ int main() {
                 printf("Nombre de usuario o contraseña incorrectos. Te quedan %d intentos.\n", intentos);
             } else {
                 printf("Se ha superado el número de intentos permitido.\n");
-                break;
+                return 1; 
             }
         }
     } while (intentos > 0);
 
-    printf("Bienvenido al sistema de facturación.\n");
+  
+    printf("\nAhora vamos a calcular el importe de la factura.\n");
     printf("Por favor introduce la cantidad y el precio unitario de cada artículo.\n");
     printf("Cuando hayas terminado, introduce 0 en la cantidad para finalizar.\n\n");
 
-    do {
-        printf("Cantidad: ");
-        scanf("%d", &cantidad);
-        if (cantidad == 0) {
-            break; 
-        }
-
+   
+    printf("Cantidad: ");
+    scanf("%d", &cantidad);
+    while (cantidad != 0) {
         printf("Precio unitario: ");
         scanf("%f", &precioUnitario);
 
-       
         if (cantidad < 0 || precioUnitario < 0) {
             printf("Error: la cantidad y el precio deben ser números positivos.\n\n");
-            continue; 
+        } else {
+            float importe = cantidad * precioUnitario;
+            importeTotal += importe;
         }
 
-        
-        float importe = cantidad * precioUnitario;
-        importeTotal += importe;
-
-    } while (cantidad != 0);
+        printf("\nCantidad: ");
+        scanf("%d", &cantidad);
+    }
+    
 
     
+
     printf("\nImporte total de la factura: %.2f\n", importeTotal);
 
     return 0;
-
 }
